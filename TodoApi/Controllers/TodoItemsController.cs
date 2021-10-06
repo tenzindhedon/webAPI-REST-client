6cd0569c3464c089277c1fecefd51afe5076a1b9
tenzindhedon <tendhedon@gmail.com>
2021-10-06 13:32:44 -0300

    Added DeleteTodoItem

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -72,5 +72,21 @@ namespace TodoApi.Controllers
 
             return NoContent();
         }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }

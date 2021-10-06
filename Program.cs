45934ba41e2e104b7c50e1883f573e55315cb15c
tenzindhedon <tendhedon@gmail.com>
2021-10-06 13:49:57 -0300

    Udated program.cs class with http methods

--- Program.cs
+++ Program.cs
@@ -9,35 +9,103 @@ namespace WebAPIClient
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        static HttpClient client = new HttpClient();
 
-        static async Task Main(string[] args)
+        static void ShowProduct(Product product)
         {
-            var repositories = await ProcessRepositories();
-
-            foreach (var repo in repositories)
-            {
-                Console.WriteLine(repo.Name);
-                Console.WriteLine(repo.Description);
-                Console.WriteLine(repo.GitHubHomeUrl);
-                Console.WriteLine(repo.Homepage);
-                Console.WriteLine(repo.Watchers);
-                Console.WriteLine(repo.LastPush);
-                Console.WriteLine();
-            }
+            Console.WriteLine($"Name: {product.Name}\tPrice: " +
+                $"{product.Price}\tCategory: {product.Category}");
         }
 
-        private static async Task<List<Repository>> ProcessRepositories()
+        static async Task<Uri> CreateProductAsync(Product product)
         {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/products", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        static async Task<Product> UpdateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Product>();
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/products/{id}");
+            return response.StatusCode;
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:64195/");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+                new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            try
+            {
+                // Create a new product
+                Product product = new Product
+                {
+                    Name = "Gizmo",
+                    Price = 100,
+                    Category = "Widgets"
+                };
 
-            return repositories;
+                var url = await CreateProductAsync(product);
+                Console.WriteLine($"Created at {url}");
+
+                // Get the product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Update the product
+                Console.WriteLine("Updating price...");
+                product.Price = 80;
+                await UpdateProductAsync(product);
+
+                // Get the updated product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(product.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
